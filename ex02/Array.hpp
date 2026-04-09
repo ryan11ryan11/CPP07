a13e750d82e370b943f74eda07b4c2700340eebc
jjjiiio <Junhwa.Hong@gmx.de>
2026-04-09 22:36:41 +0200

    second

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -3,34 +3,29 @@ template <typename T>
 class Array {
 	private:
 		T *_arr;
-		size_t len;
+		size_t _len;
 
 	public:
-		Array() {
+		Array() :_len(0) {
 			this->_arr = new T[]();
 		};
-		Array(unsigned int n) {
+		Array(unsigned int n) :_len(n) {
 			this->_arr = new T[n]();
 		}
 		~Array() {
 			delete[] _arr;
 		}
-		Array(const Array &a) {
-			size_t len = 0;
-			while(a._arr[len]) {
-				len ++;
-			}
-			this->_arr = new T[n]();
-			for (size_t i = 0; i < len; i++) {
+		Array(const Array &a) :_len(a._len) {
+			size_t n;
 
-			}
-			while(a._arr[i]) {
+			n = a._len;
+			this->_len = n;
+			this->_arr = new T[n]();
+			for (size_t i = 0; i < n) {
 				this->_arr[i] = a._arr[i];
-				i ++;
 			}
-			this->_arr = temp;
 		}
-		size_t size() {
+		size_t size() const {
 			return (this->len);
 		}
 };
