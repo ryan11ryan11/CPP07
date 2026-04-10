9e41252da208a5b9ba2746cd5dbf8fc9de0b2fa7
Junhwa Hong <junhhong@c1r9s1.42wolfsburg.de>
2026-04-10 14:22:24 +0200

    Final

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,10 +6,10 @@
 
 template <typename T, typename F>
 void iter(T* arr, const size_t len, const F& func) {
-    for(size_t i = 0; i < len; i++) {
-        func(arr[i]);
-    }
-    return ;
+	for(size_t i = 0; i < len; i++) {
+		func(arr[i]);
+	}
+	return ;
 }
 
 #endif
